a87570bd3b99b197a077ab4c3eecd76fb32c5a4c
Sindhura <sindhu.tiruvedula@gmail.com>
2024-09-14 15:33:58 +0530

    file added

---
+++ expense/variables.tf
@@ -0,0 +1,21 @@
+variable "instance_names" {
+    type = list(string)
+    default = ["mysql", "backend", "frontend"]
+}
+
+variable "common_tags" {
+    type = map
+    default = {
+        Project = "expense"
+        Environment = "dev"
+        Terraform = "true"
+    }
+}
+
+variable "zone_id" {
+    default = "Z0933577K8HH1ULEJCS5"
+}
+
+variable "domain_name" {
+    default = "sikadu.online"
+}
